9c4f58f163049c9f0bbe6c96708a89d9b327ecb6
Samuel <samuel.deoliveiranevess@gmail.com>
2026-07-08 01:00:45 -0300

    Revert "Correção tela Sinal"

--- app/Services/CorrecaoDeSinalService.php
+++ app/Services/CorrecaoDeSinalService.php
@@ -64,6 +64,9 @@ class CorrecaoDeSinalService
         }
 
         $setor = trim((string) ($dados['setor'] ?? ''));
+        if ($setor === '') {
+            $setor = trim((string) ($dados['localizacao_texto'] ?? ''));
+        }
 
         $codigoExibicao = trim((string) ($dados['taskCode'] ?? ''));
         if ($codigoExibicao === '' && str_contains($titulo, '·')) {
@@ -102,13 +105,6 @@ class CorrecaoDeSinalService
 
     public function createCorrecaoDeSinal(array $dados): OpTask
     {
-        if (isset($dados['elemento'])) {
-            $elemento = trim((string) $dados['elemento']);
-            if ($elemento !== '') {
-                $dados['setor'] = $elemento;
-            }
-            unset($dados['elemento']);
-        }
         $dados = OpTask::filtrarEntradaCliente($dados);
         $dados['categoria'] = 'correcao-atenuacao';
         $dados['taskCode'] = $this->opTaskService->gerarTaskCode($dados);
@@ -177,13 +173,6 @@ class CorrecaoDeSinalService
     public function updateCorrecaoDeSinal(OpTask $correcao, array $dados): OpTask
     {
         $statusAnterior = $correcao->status;
-        if (isset($dados['elemento'])) {
-            $elemento = trim((string) $dados['elemento']);
-            if ($elemento !== '') {
-                $dados['setor'] = $elemento;
-            }
-            unset($dados['elemento']);
-        }
         $dados = OpTask::filtrarEntradaCliente($dados);
 
         if (isset($dados['status']) && $dados['status'] === 'Finalizada') {
